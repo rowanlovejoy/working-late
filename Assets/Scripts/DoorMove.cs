6e7e0ead380f89af50977d408c408095879d6eca
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-10-20 15:25:00 +0100

    Implemented system to detect if player is in range and open a door

---
+++ Assets/Scripts/DoorMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMove : MonoBehaviour
+{
+    [SerializeField]
+    private DoorTrigger m_doorTrigger;
+
+    private BoxCollider m_boxCollider;
+    private MeshRenderer m_meshRenderer;
+
+    private void Awake()
+    {
+        m_doorTrigger = GameObject.Find("DoorTrigger").GetComponent<DoorTrigger>();
+        m_boxCollider = GetComponent<BoxCollider>();
+        m_meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (m_doorTrigger.playerInRange && m_boxCollider.enabled && m_meshRenderer.enabled)
+        {
+            m_boxCollider.enabled = false;
+            m_meshRenderer.enabled = false;
+        }
+        else if (!m_doorTrigger.playerInRange && !m_boxCollider.enabled && !m_meshRenderer.enabled)
+        {
+            m_boxCollider.enabled = true;
+            m_meshRenderer.enabled = true;
+        }
+	}
+}
---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public bool playerInRange = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInRange = false;
+    }
+}
