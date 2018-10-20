48afa50ac350d3c275c860f177ad8c6538555eda
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2018-10-20 11:57:49 +0100

    Implemented player character with movement and first-person mouse look

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The code in this script is based on a publically available YouTube tutorial produced by user Acacia Developer (2018)
+ * For the full reference to this source, along with others used for this Design Challenge 01, see Documentation\Design Challenge 01\3rd Party Assets\Design Challenge 01 Reference List.pdf
+ * */
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private float m_mouseSensitivity = 150.0f; // Controls the amount the persective moves when the player moves their cursor
+
+    [SerializeField]
+    private Transform m_playerBody; // Reference to the player character's Transform component
+
+    private float m_xAxisClamp = 0.0f; // Stores players accumulated view rotation on the x-axis
+
+    // Locks the player's cursor immediately
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+	void Update()
+    {
+        CameraRotation();
+	}
+
+    // Locks the player's cursor to the centre of the screen and hides it from view
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Rotates the player's view based on their mouse cursor's movement
+    private void CameraRotation()
+    {
+        // Gets and stores input from the player's mouse, scaling it based on mouse sensitivy and frame rate
+        float _mouseX = Input.GetAxisRaw("Mouse X") * m_mouseSensitivity * Time.deltaTime; 
+        float _mouseY = Input.GetAxisRaw("Mouse Y") * m_mouseSensitivity * Time.deltaTime;
+
+        // Accumlates to the player's mouse cursor y-axis movement. If amount exceeds certain values, calls method to prevent further rotation in that direction
+        m_xAxisClamp += _mouseY; 
+        if (m_xAxisClamp > 90.0f)
+        {
+            m_xAxisClamp = 90.0f;
+            _mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (m_xAxisClamp < -90.0f)
+        {
+            m_xAxisClamp = -90.0f;
+            _mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        // Rotates the player's view
+        transform.Rotate(Vector3.left * _mouseY);
+        m_playerBody.Rotate(Vector3.up * _mouseX);
+    }
+
+    // Clamps the rotation on the x axis, preventing the the player from rotating 360 degrees on that axis
+    private void ClampXAxisRotationToValue(float _value)
+    {
+        Vector3 _eulerRoation = -transform.eulerAngles;
+        _eulerRoation.x = _value;
+        transform.eulerAngles = _eulerRoation;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * The code in this script is based on a publically available YouTube tutorial produced by user Acacia Developer (2018)
+ * For the full reference to this source, along with others used for this Design Challenge 01, see Documentation\Design Challenge 01\3rd Party Assets\Design Challenge 01 Reference List.pdf
+ * */
+
+public class PlayerMove : MonoBehaviour
+{
+    private CharacterController m_charController; // Reference to the player character's CharacterController component
+
+    [SerializeField]
+    private float m_movementSpeed = 10.0f; // Controls how quickly the player moves in a direction
+
+    private void Awake()
+    {
+        m_charController = GetComponent<CharacterController>(); // Gets and stores a reference to the player character's CharacterController component
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+    }
+
+    // Moves the player character in world based on user input
+    void PlayerMovement()
+    {
+        // Gets and stores the player's vertical and horizontal input
+        float _verticalInput = Input.GetAxis("Vertical");
+        float _horizInput = Input.GetAxis("Horizontal");
+
+        // Stores amount to move on player character's forward and right vectors
+        Vector3 _forwardMovement = transform.forward * _verticalInput;
+        Vector3 _rightMovement = transform.right * _horizInput;
+
+        // Uses CharacterController component to move player based on forward and right vectors.
+        m_charController.SimpleMove(Vector3.ClampMagnitude(_forwardMovement + _rightMovement, 1.0f) * m_movementSpeed);
+    }
+}
